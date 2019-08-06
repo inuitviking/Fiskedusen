17c261a0e44773a3f3130b9ef45feb02b2bc6532
inuitviking <runurth@gmail.com>
2019-08-06 15:07:35 +0200

    Added properties and constructors

--- Fiskehandler/Campaign.cs
+++ Fiskehandler/Campaign.cs
@@ -1,8 +1,29 @@
 using System;
+using System.Collections.Generic;
 
 namespace Fiskehandler
 {
     public class Campaign{
+        public string Name {get;set;}
+        public DateTime Start {get;private set;}
+        public DateTime End {get;private set;}
+        public double Rate {get;set;}
+        public List<Category> Categories {get;set;}
 
+        /// <summary>
+        /// Creates a new campaign
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <param name="rate"></param>
+        /// <param name="categories"></param>
+        public Campaign(string name, DateTime start, DateTime end, double rate, List<Category> categories){
+            this.Name = name;
+            this.Start = start;
+            this.End = end;
+            this.Rate = rate;
+            this.Categories = categories;
+        }
     }
 }
--- Fiskehandler/Category.cs
+++ Fiskehandler/Category.cs
@@ -3,6 +3,14 @@ using System;
 namespace Fiskehandler
 {
     public class Category{
+        public string Name {get;set;}
 
+        /// <summary>
+        /// Creates a new category
+        /// </summary>
+        /// <param name="name"></param>
+        public Category(string name){
+            this.Name = name;
+        }
     }
 }
