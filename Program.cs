131ed26467c14f9ea05ea605a615c7bbbdac020d
inuitviking <runurth@gmail.com>
2019-08-06 13:52:34 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fiskedusen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
