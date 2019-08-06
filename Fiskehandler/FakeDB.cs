8c0fd61e85c2dacbc669f292602034a535332a99
Angutivik Casper Rúnur Tausen Hansen <runurth@gmail.com>
2019-08-06 20:57:18 +0200

    Added availability for campaigns, products, and categories, and added usecase examples in Program.cs

--- Fiskehandler/FakeDB.cs
+++ Fiskehandler/FakeDB.cs
@@ -11,6 +11,9 @@ namespace Fiskehandler
         public List<Product> Products {get;set;}
         public List<Category> Categories {get;set;}
 
+        /// <summary>
+        /// Initialises the fake database (just a bunch of lists)
+        /// </summary>
         public FakeDB(){
             Customers = new List<Customer>();
             Subscriptions = new List<Subscription>();
@@ -18,5 +21,39 @@ namespace Fiskehandler
             Products = new List<Product>();
             Categories = new List<Category>();
         }
+
+        /// <summary>
+        /// Creates a new category and puts it in the FakeDB Categories List.
+        /// </summary>
+        /// <param name="name"></param>
+        public void CreateCategory(string name){
+            Category category = new Category(name);
+            this.Categories.Add(category);
+        }
+
+        /// <summary>
+        /// Creates a new campaign and puts it in the FakeDB Campaigns List.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <param name="rate"></param>
+        /// <param name="categories"></param>
+        public void CreateCampaign(string name, DateTime start, DateTime end, double rate, List<Category> categories){
+            Campaign campaign = new Campaign(name, start, end, rate, categories);
+            this.Campaigns.Add(campaign);
+        }
+
+        /// <summary>
+        /// Creates a new product and puts it in the FakeDB Products List.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="category"></param>
+        /// <param name="price"></param>
+        public void CreateProduct(string name, Category category, double price){
+            Product product = new Product(name, category, price);
+            this.Products.Add(product);
+        }
+
     }
 }
