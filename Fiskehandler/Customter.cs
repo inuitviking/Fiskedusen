45a9c1ae099546ffb1cd5d1b24e9ae8dde6a2dfe
gamingAmee <amalie2410@gmail.com>
2019-08-06 15:09:11 +0200

    Added properties and constructor to Customer and Subscription

--- Fiskehandler/Customter.cs
+++ Fiskehandler/Customter.cs
@@ -3,6 +3,23 @@ using System;
 namespace Fiskehandler
 {
     public class Customer{
+        
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Creates a new Customer
+        /// </summary>
+        /// <param name="Name"></param>
+        /// <param name="Address"></param>
+        /// <param name="Email"></param>
+        public Customer(string Name, string Address, string Email)
+        {
+            this.Name = Name;
+            this.Address = Address;
+            this.Email = Email;
+        }
 
     }
 }
